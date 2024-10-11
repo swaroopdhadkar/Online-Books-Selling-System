164cea133e5032eb1358c92ce2cd804023dbacdb
tarun974 <tarun@mavonic.com>
2024-10-11 17:03:51 +0530

    Set up Fastify server and environment variables

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const fastify = require('fastify') ({ logger : true});
+
+fastify.get ('/', async(request,reply) => {
+    return { message : 'Welcome to the Online Books Selling System'}
+})
+// Response for GET method , GET endpoint for the root path
+
+const start = async () => {
+    try {
+        await fastify.listen({ port : 3000 });                                             // Listen on PORT 3000
+        console.log('Server is running at http://localhost:3000');             // Log the server URL
+    } catch(err) {                                                               // Catch any error during server startup and log them
+        fastify.log.error(err);                                                  // Log any errors that may occur
+        process.exit(1);                                                         // Exit the process with a failure code
+    }
+
+};
+
+start();                                        // Invoke the start function
+
