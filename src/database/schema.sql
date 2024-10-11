040a60c4ceac47404a901434ba2edc35aa69fa9c
tarun974 <tarun@mavonic.com>
2024-10-11 15:16:23 +0530

    directory structure

---
+++ src/database/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15),
+    address VARCHAR(255),
+    role ENUM('Customer', 'Admin') DEFAULT 'Customer'
+);
