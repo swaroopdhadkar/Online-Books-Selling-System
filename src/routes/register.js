64f6c08a8c2d199bfb7c0254b995e8729b9929b9
swaroopdhadkar <swaroopdhadkar@gmail.com>
2024-10-24 19:53:24 +0530

    Changes for Registration API

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,37 +1,37 @@
 const User  = require('../models/userModel');                            // Import User Model
 const userService = require('../services/userService');
 const bcrypt = require('bcrypt');
-const fastify = require('fastify') ();                              // Creating new Fastify instance
 
-async function registerUser(request, reply) {
-    const { username, email, password, phone_number, address } = request.body ;
+async function registerUserRoutes(fastify, options) {
+    fastify.post('/register', async (request, reply) => {
+        const { username, email, password, phone_number, address } = request.body ;
+    
 
+        if (!username || !email || !password || !phone_number ||!address) {                                                        // Validate input
+        return reply.status(400).send({ message : 'Username, Email and Password are required'})
+        }
 
-if( !username || !email || !password || !phone_number ||!address ) {                                                        // Validate input
-    return reply.status(400).send({ message : 'Username, Email and Password are required'})
+        const hashedPassword = await bcrypt.hash(password, 10);                     // Hash password before saving
+
+        const newUser = new User({                                                  // Create new User instance
+            username,
+            email,
+             password: hashedPassword,
+            phone_number,
+            address
+    });
+
+        try{
+            // Use the service to save the User to the Database
+            const result= await userService.createUser(newUser);
+
+            // Success response
+            reply.status(201).send({ message: 'Registered Successfully!'});
+        } catch(error){
+            console.error("Error details:", error);
+            reply.status(500).send({ message: 'Error registering user', error: error.message || error});
+        }
+    });
 }
 
-const hashedPassword = await bcrypt.hash(password, 10);                     // Hash password before saving
-
-const newUser = new User({                                                  // Create new User instance
-    username,
-    email,
-    password: hashedPassword,
-    phone_number,
-    address
-});
-
-try{
-    // Use the service to save the User to the Database
-    const result= await userService.createUser(newUser);
-
-    // Success response
-    reply.status(201).send({ message: 'Registered Successfully!'});
-} catch(error){
-    console.error("Error details:", error);
-    reply.status(500).send({ message: 'Error registering user', error: error.message || error});
-}
-
-}
-
-module.exports = { registerUser };                                                  // Export function to use in Routes
+module.exports = registerUserRoutes;                                                  // Export function to use in Routes
